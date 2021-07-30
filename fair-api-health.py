bdf5cc5e34e7ce02c74f061ea5c578ef1da4715a
Ross Barnie <rossbarnie@aridhia.net>
2021-07-30 18:37:22 +0100

    FAIR-3296: Add benefits of refactor to all scripts

--- fair-api-health.py
+++ fair-api-health.py
@@ -1,25 +1,11 @@
+from common.constants import SSL_VERIFY, FAIR_API_ENDPOINT
 import requests
-import os
 
-if 'FAIR_API_TOKEN' not in os.environ:
-    print('Please add FAIR_API_TOKEN to the environment')
-    exit(1)
-
-if 'FAIR_API_ENDPOINT' not in os.environ:
-    print('Please add FAIR_API_ENDPOINT to the environment')
-    exit(1)
-
-FAIR_API_TOKEN=os.environ['FAIR_API_TOKEN']
-FAIR_API_ENDPOINT=os.environ['FAIR_API_ENDPOINT']
-https = 'https://'
-
-if FAIR_API_ENDPOINT[:5] == 'https':
-    https = ''
-
-health_endpoint = f'{https}{FAIR_API_ENDPOINT}health'
+health_endpoint = f'{FAIR_API_ENDPOINT}health'
+headers = { 'Content-Type': 'application/json'}
 print(f'Testing API endpoint: {health_endpoint}')
 
-r = requests.get(health_endpoint)
+r = requests.get(health_endpoint, headers=headers, verify=SSL_VERIFY)
 if r.status_code != 200:
     print(f'Health check failed. Status code: {r.status_code} ({r.content})')
 else:
